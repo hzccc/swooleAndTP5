d298d8b3354bd98d294ab048ab0b71b224b3eb02
hzcc <395086708@qq.com>
2019-05-15 20:52:28 +0800

    测试代码

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -3,6 +3,7 @@ namespace app\index\controller;
 use think\Controller;
 use swooleobj\SwooleServer;
 use app\common\lib\Task;
+use think\Request;
 class Index extends Controller
 {
     public function index()
@@ -19,9 +20,37 @@ class Index extends Controller
 //	$obj->cookie('test','test');
 	echo request()->action();
     }
+
+    public function login(){
+        $request = Request::instance();
+        if($request->isPost()){
+            setcookie('userInfo',$userInfo,time()+500,'/');
+            //假装登录
+            $arr = [
+                "status" => 1,
+                "info" => "登录成功",
+            ];
+            return json_encode($arr);
+        }
+    }
+
     public function checkLogin(){
-//	dump($_COOKIE['test']);
-//	echo 1;
+        $request = Request::instance();
+        if($request->isGet()){
+            if(empty($_COOKIE['userInfo'])){
+                $arr = [
+                    "status" => 0,
+                    "info" => "请重新登录",
+                ];
+                return json_encode($arr);
+            } else {
+                $arr = [
+                    "status" => 1,
+                    "info" => "登录中",
+                ];
+                return json_encode($arr);
+            }
+        }
     }
     
 }
